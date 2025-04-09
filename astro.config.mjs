6441bb8b14d45c98d82e45bd7406ecabcc2675c6
samuelyoung2200 <spy8s@missouristate.edu>
2025-04-09 18:44:15 -0500

    Third time's the charm

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://AnySpy.github.io'
+    site: 'https://AnySpy.github.io',
+    base: '/my-website/'
 });
